f3737b96803c03d03ace80817a5d7cc458c01fca
NGUYEN Quang Anh <nguyenquanganh@TheAncient.local>
2015-04-23 20:50:07 +0700

    finish 2.1

--- TestDSL/TestDSL.cpp
+++ TestDSL/TestDSL.cpp
@@ -28,7 +28,7 @@ typedef Container::const_iterator ConstIterator;
 typedef StandardDSS4Computer<ConstIterator> DSSComputer;  
 
 
-pair<Container, ConstIterator> adjustVector(Container contour){
+pair<Container, int> adjustVector(Container contour){
     DSSComputer front, back;
     Container tempt_f, tempt_b;
 
@@ -69,17 +69,18 @@ pair<Container, ConstIterator> adjustVector(Container contour){
     ConstIterator iterator_back = tempt_b.end();
 
     Container newContour;
-
+    int distance = 0;
     while (iterator_back != tempt_b.begin()){
         iterator_back--;
         newContour.push_back(*iterator_back);
+        distance++;
     }
-    ConstIterator startPoint = newContour.end()-1;
 
     newContour.insert(newContour.end(), contour.begin(), contour.end());
-    pair<Container, ConstIterator> p;
+    pair<Container, int> p;
     p.first = newContour;
-    p.second = startPoint;
+    p.second = distance - 1;
+    //cout << "Start Point :" << *startPoint << endl;
 
     return p;
 }
@@ -102,15 +103,15 @@ double calculCurvature(Container contour, ConstIterator current){
     }
 
     while((back.begin() != contour.begin()) && back.extendBack()){
-        it_b++;
-    }
-
-    if (back.begin() == contour.begin()){
         it_b--;
     }
 
-    cout << "BackBegin:" << *back.begin() << endl;
-    cout << "Back A:" << back.primitive().a() << " B:" << back.primitive().b() << " U:" << back.primitive().mu() << " W:" << back.primitive().omega() << endl;
+    if (back.begin() == contour.begin()){
+        //it_b++;
+    }
+
+    //cout << "BackBegin:" << *back.begin() << endl;
+    //cout << "Back A:" << back.primitive().a() << " B:" << back.primitive().b() << " U:" << back.primitive().mu() << " W:" << back.primitive().omega() << endl;
 
     MyPoint k = *current;
     MyPoint l = *it_b;
@@ -123,8 +124,8 @@ double calculCurvature(Container contour, ConstIterator current){
     double rayon = kl * lr * kr / sqrt((kl+lr+kr)*(kl+lr-kr)*(kl+kr-lr)*(kr+lr-kl));
     int det = (r.myArray[0]-k.myArray[0])*(l.myArray[1]-k.myArray[1]) - (l.myArray[0]-k.myArray[0])*(r.myArray[1]-k.myArray[1]);
     int sign = (det == 0) ? 0 : (det > 0) ? 1 : -1;
-    cout << "Rayon:" << rayon << " K:" << *current << " R:" << *it_f << " L:" << *it_b << endl;
-    cout << "KL:" << kl << " LR:" << lr << " KR:" << kr << endl;
+    //cout << "Rayon:" << rayon << " K:" << *current << " R:" << *it_f << " L:" << *it_b << endl;
+    //cout << "KL:" << kl << " LR:" << lr << " KR:" << kr << endl;
     return sign/rayon;
 }
 
@@ -135,32 +136,18 @@ int main()
     typedef DGtal::ImageContainerBySTLVector< DGtal::Z2i::Domain, unsigned char> Image;
     std::string filename = "../Research/images/QAcircle.pgm";
     Image image = DGtal::PGMReader<Image>::importPGM(filename);;
-    //Image image = DGtal::ITKReader<Image>::importITK(filename);
-    
+     
     Z2i::KSpace ks;
     ks.init( image.domain().lowerBound(), image.domain().upperBound(), true );
     
     Z2i::DigitalSet set2d (image.domain());
     SetFromImage<Z2i::DigitalSet>::append<Image>(set2d, image, 1, 255);
     
-    
-    //Board2D aBoard;
-    //aBoard << set2d;
-    //aBoard << image.domain();
-    
     SurfelAdjacency<2> sAdj( true );
     
     std::vector<std::vector< Z2i::Point>>  m_vector;
     Surfaces<Z2i::KSpace>::extractAllPointContours4C( m_vector, ks, set2d, sAdj);
     
-    
-    //GradientColorMap<int> cmap_grad( 0, (const int)vectContoursBdryPointels.size() );
-    //cmap_grad.addColor( Color( 50, 50, 255 ) );
-    //cmap_grad.addColor( Color( 255, 0, 0 ) );
-    //cmap_grad.addColor( Color( 255, 255, 10 ) );
-    //cmap_grad.addColor( Color( 25, 255, 255 ) );
-    //cmap_grad.addColor( Color( 255, 25, 255 ) );
-    //cmap_grad.addColor( Color( 25, 25, 25 ) );
     
     // Construction of the computer
     DSSComputer theDSSComputer;  
@@ -173,10 +160,12 @@ int main()
         contour.push_back(m_vector.at(0).at(j));
     }
 
-    pair<Container, ConstIterator> p = adjustVector(contour);
+    int size = contour.size();
+
+    pair<Container, int> p = adjustVector(contour);
     contour = p.first;
  
-    ConstIterator startPoint = p.second;
+    ConstIterator startPoint = contour.begin() + p.second;
     ConstIterator current = contour.begin();
 
     cout << "Start Point : " << *startPoint << endl;
@@ -188,10 +177,20 @@ int main()
 
     vector<double> vectorCouvature;
 
-    for (int i=0; i<10; i++){
-        //cout << *startPoint << " " << calculCurvature(contour, startPoint) << endl;
+    ConstIterator i = startPoint;
+    /*
+    while (i != contour.end()){
+        cout << "I : " << *i << endl;
+        i++;
+    }
+    */
+    
+    for (int i=0; i<size; i++){
+        cout << *startPoint << " curvature " << calculCurvature(contour, startPoint) << endl;
         startPoint++;
     }
+
+    
     /*
     theDSSComputer.init( current );
     while ( ( theDSSComputer.end() != contour.end() ) &&
@@ -199,13 +198,14 @@ int main()
         cout << "Iter " << *current << endl;
         cout << "PointFront " << theDSSComputer.front() << endl;
     }
-    
-      // Trace to the standard output
-    cout << theDSSComputer << endl;
-
-    DSSComputer::Primitive theDSS = theDSSComputer.primitive(); 
-    cout << theDSS << endl; 
     */
+
+      // Trace to the standard output
+    //cout << theDSSComputer << endl;
+
+    //DSSComputer::Primitive theDSS = theDSSComputer.primitive(); 
+    //cout << theDSS << endl; 
+    
     
     //aBoard.saveEPS("freemanChainFromImage.eps");
     return 0;
