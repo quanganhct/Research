db2919a24173267dc97773da66482d4935c6cb03
NGUYEN Quang Anh <nguyenquanganh@TheAncient.local>
2015-04-27 19:39:36 +0700

    cmake

--- OPENCL/hello.cpp
+++ OPENCL/hello.cpp
@@ -1,11 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
  
-#ifdef __APPLE__
+
 #include <OpenCL/opencl.h>
-#else
-#include <CL/cl.h>
-#endif
  
 #define MEM_SIZE (128)
 #define MAX_SOURCE_SIZE (0x100000)
